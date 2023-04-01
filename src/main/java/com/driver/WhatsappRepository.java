f062f92428aeef0e8f2e8ab933675b9522d5647a
unknown <hmanojacharya@gmail.com>
2023-04-01 14:48:54 +0530

    whatsapp

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -28,6 +28,7 @@ public class WhatsappRepository {
         this.messageId = 0;
     }
 
+    //  creating user
     public String createUser(String name, String mobile) throws Exception {
         if(userMobile.contains(mobile)) {
             throw new Exception("User already exists");
@@ -38,6 +39,7 @@ public class WhatsappRepository {
         return "SUCCESS";
     }
 
+    //
     public Group createGroup(List<User> users){
         if(users.size()==2){
             Group  group=new Group(users.get(1).getName(),2);
